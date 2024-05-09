512ad52c0876ef0f5e397c3edf7b6748ecf0b785
Aviv jan <avivjan2000@gmail.com>
2024-05-09 15:09:21 +0300

    {"use_cache": False}

--- core/task_vectors.py
+++ core/task_vectors.py
@@ -30,7 +30,7 @@ def run_icl(
 ) -> List[str]:
     format_dataset_kwargs = {"include_train": include_train}
     inputs = tokenize_datasets(tokenizer, test_datasets, format_dataset_kwargs=format_dataset_kwargs)
-    new_ids = batch_generate(model, tokenizer, inputs=inputs, generate_kwargs={"max_new_tokens": 1})
+    new_ids = batch_generate(model, tokenizer, inputs=inputs, generate_kwargs={"max_new_tokens": 1, "use_cache": False})
     predictions = decode_predictions(new_ids, tokenizer)
 
     return predictions
